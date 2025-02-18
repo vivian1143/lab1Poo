400b3cba7b65b860651141bb1d208be9890fe97b
vivian1143 <vaprado@unillanos.edu.co>
2025-02-18 08:54:02 -0500

    primer commit

---
+++ src/main/java/com/mycompany/lab1poo/Lab1Poo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab1poo;
+
+/**
+ *
+ * @author Estudiante_MCA
+ */
+public class Lab1Poo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
